d9996d344ff7c8fbb465462463111c983a4ebbcd
Lee Eunchan <ym4315@nate.com>
2023-06-22 19:53:41 +0900

    update router

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const signupRouter = require("./routes/signups.js");
 const loginRouter = require("./routes/login.js");
 
 const connect = require("./schema/index.js");
+const cookieParser = require("cookie-parser");
 
 connect();
 // 전역객체 body parser에 사용
 
 // 미들웨어
+
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api", [postsRouter, commentRouter, signupRouter, loginRouter]);
 
 app.listen(port, () => {
