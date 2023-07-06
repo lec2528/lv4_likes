3d032d948053e8dbe92e341fae8a866d8959d800
Lee Eunchan <ym4315@nate.com>
2023-07-06 17:45:52 +0900

    fix:미들웨어 파일 수정

--- Middleware/loginmiddleware.js
+++ Middleware/loginmiddleware.js
@@ -1,26 +1,26 @@
-const jwt = require("jsonwebtoken");
-const { Users } = require("../models");
+const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
 
 module.exports = async (req, res, next) => {
   const { Authorization } = req.cookies;
-  const [loginType, loginToken] = (Authorization ?? "").split(" ");
+  const [loginType, loginToken] = (Authorization ?? '').split(' ');
 
-  if (!loginToken || loginType !== "Bearer") {
+  if (!loginToken || loginType !== 'Bearer') {
     res.status(401).send({
-      errorMessage: "로그인 후 이용 가능한 기능입니다11111.",
+      errorMessage: '로그인 후 이용 가능한 기능입니다11111.',
     });
     return;
   }
 
   try {
-    const { userId } = jwt.verify(loginToken, "custom-secret-key");
+    const { userId } = jwt.verify(loginToken, 'custom-secret-key');
     const user = await Users.findOne(userId);
     res.locals.signin = user;
     next();
   } catch (err) {
     console.error(err);
     res.status(401).send({
-      errorMessage: "로그인 후 이용 가능한 기능입니다.",
+      errorMessage: '로그인 후 이용 가능한 기능입니다.',
     });
   }
 };
