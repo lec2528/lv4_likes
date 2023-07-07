0ace02368ada26950326601eb2b575d739037bf1
Lee Eunchan <ym4315@nate.com>
2023-07-07 22:52:17 +0900

    feat:like model 생성 좋아요 기능 구현을 위한 like 모델을 생성하고 sequelize를 이용한 mysql 테이블을 생성 했습니다.

---
+++ models/likes.js
@@ -0,0 +1,33 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Likes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Users, {
+        targetKey: 'userId',
+        foreignKey: 'userId',
+      });
+      this.belongsTo(models.Posts, {
+        targetKey: 'postId',
+        foreignKey: 'postId',
+      });
+    }
+  }
+  Likes.init(
+    {
+      userId: DataTypes.INTEGER,
+      postId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Likes',
+    }
+  );
+  return Likes;
+};
