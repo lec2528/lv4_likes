39051dba394e42d2d36f6821a30448594af0096f
Lee Eunchan <ym4315@nate.com>
2023-07-09 23:37:11 +0900

    fix:varify 부분 수정 userId값이 라우터로 전달이 안되어서 코드 수정 후 정상적으로 전달 확인

--- Middleware/loginmiddleware.js
+++ Middleware/loginmiddleware.js
@@ -4,7 +4,6 @@ const { Users } = require('../models');
 module.exports = async (req, res, next) => {
   const { Authorization } = req.cookies;
   const [loginType, loginToken] = (Authorization ?? '').split(' ');
-
   if (!loginToken || loginType !== 'Bearer') {
     res.status(401).send({
       errorMessage: '로그인 후 이용 가능한 기능입니다11111.',
@@ -13,9 +12,19 @@ module.exports = async (req, res, next) => {
   }
 
   try {
-    const { userId } = jwt.verify(loginToken, 'custom-secret-key');
-    const user = await Users.findOne(userId);
-    res.locals.signin = user.userId;
+    const decodedToken = jwt.verify(loginToken, 'custom-secret-key');
+    const userId = decodedToken.userId;
+    console.log(userId);
+
+    const user = await Users.findOne({ where: { userId } });
+    if (!user) {
+      res.clearCookie('Authorization');
+      return res
+        .status(401)
+        .json({ message: '토큰 사용자가 존재하지 않습니다.' });
+    }
+    res.locals.user = user;
+    console.log(res.locals.user);
     next();
   } catch (err) {
     console.error(err);
