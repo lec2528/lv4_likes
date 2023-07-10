3d0064d74ba5a67db0fbb7aadd3175b4ec22a9a4
Lee Eunchan <ym4315@nate.com>
2023-07-10 21:36:07 +0900

    fix: 주석 작업 및 사용하지 않는 코드 삭제 각 기능별 설명과 console.log등의 코드 삭제 try catch문 적용하여 에러핸들링 되도록 수정

--- routes/comments.js
+++ routes/comments.js
@@ -5,79 +5,94 @@ const middleware = require('../Middleware/loginmiddleware.js');
 
 //댓글 생성
 router.post('/post/:postId/comment', middleware, async (req, res) => {
-  const { userId } = res.locals.user;
-  console.log('userId', userId);
-  const { postId } = req.params;
-  const { nickname, content } = req.body;
+  try {
+    const { userId } = res.locals.user;
+    const { postId } = req.params;
+    const { nickname, content } = req.body;
 
-  const isExistPost = await Posts.findOne({ where: { postId } });
-  if (!isExistPost) {
-    res.status(412).json({
-      errorMessage:
-        '게시글이 존재하지 않거나 삭제되었기 때문에 댓글을 생성할 수 없습니다.',
+    //댓글을 작성하려는 게시글이 존재하는지 확인합니다.
+    const isExistPost = await Posts.findOne({ where: { postId } });
+    if (!isExistPost) {
+      res.status(412).json({
+        errorMessage:
+          '게시글이 존재하지 않거나 삭제되었기 때문에 댓글을 생성할 수 없습니다.',
+      });
+    }
+
+    if (!content.length) {
+      res.status(412).json({
+        errorMessage: '댓글 내용을 입력해주세요',
+      });
+    }
+    // create 메소드를 사용하여 게시글을 생성하고 게시물의 postId와 작성자의 userId를 가져와 저장합니다.
+    const comments = await Comments.create({
+      postId,
+      userId,
+      nickname,
+      content,
+      createAt: Date.now(),
     });
+
+    res.status(200).json({ comments, result: '댓글이 생성 되었습니다.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
   }
-
-  if (!content.length) {
-    res.status(412).json({
-      errorMessage: '댓글 내용을 입력해주세요',
-    });
-  }
-
-  const comments = await Comments.create({
-    postId,
-    userId,
-    nickname,
-    content,
-    createAt: Date.now(),
-  });
-
-  res.status(200).json({ comments, result: '댓글이 생성 되었습니다.' });
 });
 
 // 댓글 조회
 router.get('/post/:postId/comment', async (req, res) => {
-  const comment = await Comments.findAll({
-    where: { postId: req.params.postId },
-    attributes: [
-      'commentId',
-      'postId',
-      'userId',
-      'nickname',
-      'content',
-      'createdAt',
-    ],
-    order: [['createdAt', 'DESC']],
-  });
-  res.status(200).json({ comment });
+  try {
+    const comment = await Comments.findAll({
+      where: { postId: req.params.postId },
+      attributes: [
+        'commentId',
+        'postId',
+        'userId',
+        'nickname',
+        'content',
+        'createdAt',
+      ],
+      order: [['createdAt', 'DESC']],
+    });
+    res.status(200).json({ comment });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
+  }
 });
 
 // 댓글 수정
 router.put('/post/:postId/comment/:commentId', middleware, async (req, res) => {
-  const { commentId } = req.params;
-  const { nickname, content } = req.body;
+  try {
+    const { commentId } = req.params;
+    const { nickname, content } = req.body;
 
-  const isExistComment = await Comments.findOne({
-    where: { commentId },
-  });
-  console.log(isExistComment);
-  if (!isExistComment) {
-    return res.status(400).json({
-      success: false,
-      errorMessage: '해당 댓글이 존재하지 않습니다.',
+    const isExistComment = await Comments.findOne({
+      where: { commentId },
     });
-  }
+    console.log(isExistComment);
+    if (!isExistComment) {
+      return res.status(400).json({
+        success: false,
+        errorMessage: '해당 댓글이 존재하지 않습니다.',
+      });
+    }
 
-  if (!content.length) {
-    res.status(412).json({
-      errorMessage: '수정할 내용을 입력해주세요',
-    });
+    if (!content.length) {
+      res.status(412).json({
+        errorMessage: '수정할 내용을 입력해주세요',
+      });
+    }
+    const updateComment = await Comments.update(
+      { nickname, content, updateAt: Date.now() },
+      { where: { commentId } }
+    );
+    res.status(200).json({ updateComment, success: '댓글이 수정되었습니다.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
   }
-  const updateComment = await Comments.update(
-    { nickname, content, updateAt: Date.now() },
-    { where: { commentId } }
-  );
-  res.status(200).json({ updateComment, success: '댓글이 수정되었습니다.' });
 });
 
 //댓글 삭제
@@ -85,17 +100,23 @@ router.delete(
   '/post/:postId/comment/:commentId',
   middleware,
   async (req, res) => {
-    const { commentId } = req.params;
-
-    const isExistComment = await Comments.findOne({ where: { commentId } });
-    if (!isExistComment) {
-      return res.status(400).json({
-        success: false,
-        errorMessage: '삭제할 댓글이 없습니다.',
-      });
+    try {
+      const { commentId } = req.params;
+      // 삭제하려는 댓글이 존재하는지 확인합니다.
+      const isExistComment = await Comments.findOne({ where: { commentId } });
+      if (!isExistComment) {
+        return res.status(400).json({
+          success: false,
+          errorMessage: '삭제할 댓글이 없습니다.',
+        });
+      }
+      // 댓글을 확인하고 destor 메소드를 이용하여 삭제합니다.
+      await Comments.destroy({ where: { commentId } });
+      res.status(200).json({ success: '댓글이 삭제되었습니다.' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ errorMessage: '서버 오류' });
     }
-    await Comments.destroy({ where: { commentId } });
-    res.status(200).json({ success: '댓글이 삭제되었습니다.' });
   }
 );
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -6,94 +6,122 @@ const loginmiddleware = require('../Middleware/loginmiddleware.js');
 
 //게시글 조회
 router.get('/post', async (req, res) => {
-  const posts = await Posts.findAll({ where: {} });
-  if (!posts) {
-    res.status(400).json({
-      errorMessage: '게시글이 없습니다.',
-    });
+  try {
+    const posts = await Posts.findAll({ where: {} });
+    if (!posts) {
+      res.status(400).json({
+        errorMessage: '게시글이 없습니다.',
+      });
+    }
+    res.status(200).json({ posts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
   }
-  res.status(200).json({ posts });
 });
 
 //게시글 생성 POST
 router.post('/post', loginmiddleware, async (req, res) => {
-  const { userId } = res.locals.user;
-  console.log('user', userId);
-  const { title, content, nickname, password } = req.body;
+  try {
+    const { userId } = res.locals.user;
+    const { title, content, nickname, password } = req.body;
 
-  const verify = await Posts.findOne({ nickname, password });
+    const verify = await Posts.findOne({ nickname, password });
 
-  if (!verify) {
-    res.status(412).json({
-      errorMessage: '게시글을 작성하려면 로그인 해주세요',
+    if (!verify) {
+      res.status(412).json({
+        errorMessage: '게시글을 작성하려면 로그인 해주세요.',
+      });
+    }
+    if (!content) {
+      res.status(412).json({
+        errorMessage: '게시글을 내용을 입력해주세요.',
+      });
+    }
+
+    const post = await Posts.create({
+      userId,
+      nickname,
+      password,
+      title,
+      content,
+      careatedAt: new Date(),
     });
-  }
-  if (!content) {
-    res.status(412).json({
-      errorMessage: '게시글을 작성해주세요',
+
+    res.status(201).json({
+      post,
+      message: '게시글을 생성하였습니다.',
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
   }
-
-  const post = await Posts.create({
-    userId,
-    nickname,
-    password,
-    title,
-    content,
-    careatedAt: new Date(),
-  });
-
-  res.status(201).json({
-    post,
-    message: '게시글을 생성하였습니다.',
-  });
 });
 //--------------------------------------------------------
 
 //게시글 상세 조회 GET
 router.get('/post/:postId', async (req, res) => {
-  const { postId } = req.params;
-  console.log({ postId });
-  const detailpost = await Posts.findAll({ where: { postId } });
-  if (!detailpost.length) {
-    return res.json({
-      errorMessage: '해당 게시글이 존재하지 않습니다.',
-    });
+  try {
+    const { postId } = req.params;
+    console.log({ postId });
+    //api로 호출된 postid만 조회합니다.
+    const detailpost = await Posts.findAll({ where: { postId } });
+    if (!detailpost.length) {
+      return res.json({
+        errorMessage: '해당 게시글이 존재하지 않습니다.',
+      });
+    }
+    res.status(200).json({ detailpost }); //posts: [result]
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
   }
-  res.status(200).json({ detailpost }); //posts: [result]
 });
 // 게시글 수정
 router.put('/post/:postId', loginmiddleware, async (req, res) => {
-  const { postId } = req.params;
-  console.log(postId);
-  const { nickname, title, content, updatedAt } = req.body;
+  try {
+    const { postId } = req.params;
+    console.log(postId);
+    const { nickname, title, content, updatedAt } = req.body;
 
-  const isExistPost = await Posts.findOne({ where: { postId: postId } });
-  if (!isExistPost) {
-    return res.status(412).json({
-      errorMessage: '수정할 게시글이 존재하지 않습니다.',
-    });
+    //where문을 사용해서 postId와 일치하는 게시물을 찾습니다.
+    const isExistPost = await Posts.findOne({ where: { postId: postId } });
+    if (!isExistPost) {
+      return res.status(412).json({
+        errorMessage: '수정할 게시글이 존재하지 않습니다.',
+      });
+    }
+    //update 메소드를 사용하여 api로 호출된 postId를 수정하고 updatedAt을 새로운 시간으로 업데이트 합니다.
+    await Posts.update(
+      { nickname, title, content, updatedAt: new Date() },
+      { where: { postId } }
+    );
+    res.status(200).json({ success: '게시글이 수정되었습니다.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
   }
-  await Posts.update(
-    { nickname, title, content, updatedAt: new Date() },
-    { where: { postId } }
-  );
-  res.status(200).json({ success: '게시글이 수정되었습니다.' });
 });
 
 //게시글 삭제
 router.delete('/post/:postId', loginmiddleware, async (req, res) => {
-  const { postId } = req.params;
-  console.log(postId);
-  const isExistPost = await Posts.findOne({ where: { postId: postId } });
-  if (!isExistPost) {
-    return res.status(400).json({
-      errorMessage: '이미 삭제되었거나 존재하지 않습니다.',
-    });
-  }
+  try {
+    const { postId } = req.params;
 
-  await Posts.destroy({ where: { postId } });
-  res.status(200).json({ success: '데이터 삭제에 성공했습니다.' });
+    //where문을 사용해서 postId와 일치하는 게시물을 찾습니다.
+    const isExistPost = await Posts.findOne({ where: { postId: postId } });
+    if (!isExistPost) {
+      return res.status(400).json({
+        errorMessage: '이미 삭제되었거나 존재하지 않습니다.',
+      });
+    }
+    //destroy 메소드를 사용하여 api로 호출된 postId를 삭제합니다.
+    await Posts.destroy({ where: { postId } });
+    res.status(200).json({ success: '데이터 삭제에 성공했습니다.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ errorMessage: '서버 오류' });
+  }
 });
 
 module.exports = router;
